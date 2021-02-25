aabda0db3ae7fa463a32d602d09fae18228c86ad
Rome Bell <romebell5@gmail.com>
2021-02-25 12:34:55 -0800

    App: Add styling and import FruitContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // imports
+import FruitContainer from './components/FruitContainer';
 
 function App() {
+  const fruits = ['banana 🍌', 'watermelon 🍉', 'cherry 🍒', 'guava', 'apple 🍎', 'kiwi 🥝', 'strawberry 🍓', 'mango 🥭', 'pineapple 🍍', 'avocado 🥑'];
   return (
     <div>
       <h1>Fruit Filter</h1>
+      <FruitContainer fruits={fruits}/>
     </div>
   );
 }
